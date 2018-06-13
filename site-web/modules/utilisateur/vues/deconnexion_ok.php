9f995ee5de713d6954dbd9acb88240d5e334d6e4
prostain <peterson.rostain@gmail.com>
2018-06-13 15:48:44 +0200

    modification panier

--- site-web/modules/utilisateur/vues/deconnexion_ok.php
+++
@@ -1,3 +0,0 @@
-<h2>Confirmation de d&eacute;connexion</h2>
-<p>Au revoir !<br />
-Vous &ecirc;tes maintenant d&eacute;connect&eacute; !</p>
--- site-web/modules/utilisateur/vues/erreur_authentification.php
+++
@@ -1,4 +0,0 @@
-<div class="alert alert-danger alert-dismissible">
-    <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-    <strong>Erreur!</strong> <?php echo $_SESSION['messageErreur']; ?>
-  </div>
